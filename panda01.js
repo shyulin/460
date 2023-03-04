183e16d546bd7d8d70e059592c42b94391b0343a
shyulin <54575199+shyulin@users.noreply.github.com>
2023-03-04 17:09:29 +0000

    Update panda01.js

--- panda01.js
+++ panda01.js
@@ -1,6 +1,6 @@
 /* 
 [rewrite_local]
-^https:\/\/tw\.usehurrier\.com\/api\/mobile\/v2\/state\/999999999*\?source\=Delivery\&update\_trigger\=pull\_down\_gesture url script-response-body panda01.js
+^https:\/\/tw\.usehurrier\.com\/api\/mobile\/v2\/state\/999999999*\?source\=Delivery\&update\_trigger\=pull\_down\_gesture url script-response-body https://raw.githubusercontent.com/shyulin/460/master/panda01.js
 [mitm]
 hostname = tw.usehurrier.com
 
