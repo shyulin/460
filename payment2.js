1988ad8ff4532d02869abc91e518fa3c46f115a9
shyulin <54575199+shyulin@users.noreply.github.com>
2023-03-19 17:10:25 +0000

    Update payment2.js

--- payment2.js
+++ payment2.js
@@ -16,7 +16,7 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 var paymenth = obj.payment_per_hour ;
-const tdistance = status_d.find(${obj.total_distance}) ;
+const tdistance = status_d.find(obj.total_distance) ;
 //const status1 = status_d.find( obj.route.deliveries[0].status)
 var status_d = {0:"進單中", near_pickup:"接近取餐點", accepted:"已接單", picked_up:"已取餐", left_pickup:"已取餐出發", near_dropoff:"接近指定點", dispatched:"預備單"};
 
