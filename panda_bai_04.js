885262b768ffabd47d1d6555c2239f5509db648b
shyulin <54575199+shyulin@users.noreply.github.com>
2023-03-28 17:27:48 +0000

    Update panda_bai_04.js

--- panda_bai_04.js
+++ panda_bai_04.js
@@ -22,7 +22,7 @@ var obj = JSON.parse(body);
 var no = obj.route.deliveries.length ;
 var dmode = obj.courier.dispatching_mode ;
 
-var status_d = {courier_notified:"進單中", near_pickup:"接近取餐店家", accepted:"已接單", picked_up:"已取餐", left_pickup:"已取餐出發", near_dropoff:"接近放參位置", dispatched:"預備單"};
+var status_d = {courier_notified:"進單中", near_pickup:"接近取餐店家", accepted:"已接單", picked_up:"已取餐", left_pickup:"已取餐出發", near_dropoff:"接近放餐位置", dispatched:"預備單"};
 
 if (no === 1 ) {
   //      const status1 = obj.route.deliveries[0].status ;
